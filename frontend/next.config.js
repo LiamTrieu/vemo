ceb39b99dbde41f0c0eced1188d287eee4987d30
LiamTrieu <gaupanda567@gmail.com>
2025-10-27 17:19:43 +0900

    fix build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
-// next.config.js
-const runtimeCaching = require('@ducanh2912/next-pwa/cache');
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   skipWaiting: true,
-  runtimeCaching, // ✅ thêm dòng này
   disable: process.env.NODE_ENV === "development",
 });
 
