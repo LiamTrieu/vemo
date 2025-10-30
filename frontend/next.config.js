94d03984b1f042056003e2a7e7657cf1ec258eb1
LiamTrieu <gaupanda567@gmail.com>
2025-10-30 18:17:27 +0900

    Revert "test"
    This reverts commit a4fde6b4f8cc3d7bff2a5acd052a588a94073bd6.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: false,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
@@ -11,8 +11,8 @@ module.exports = withPWA({
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
