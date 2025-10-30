7e6938f9c5d94ce1a1128d9626bd372bfd905cf3
LiamTrieu <gaupanda567@gmail.com>
2025-10-30 12:23:43 +0900

    fix load

--- frontend/src/app/components/Splash.tsx
+++ frontend/src/app/components/Splash.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Splash() {
   return (
-    <div className="animate-hidden-splash bg-background fixed inset-0 z-100 flex flex-col items-center justify-center">
+    <div className="animate-hidden-splash bg-background fixed inset-0 z-100 flex h-screen w-screen flex-col items-center justify-center">
       <Image
         src="/icon-512x512.png"
         width={150}
